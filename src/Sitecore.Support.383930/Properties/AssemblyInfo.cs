5f428f4ce741fb297289003eb179c1d62bcfedab
Andriy Kravets <andriy.kravets@sitecore.com>
2020-01-06 16:43:52 +0200

    Init commit

---
+++ src/Sitecore.Support.383930/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.383930")]
+[assembly: AssemblyProduct("Sitecore.Support.383930")]
+[assembly: ComVisible(false)]
